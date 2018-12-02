2f8d79bc7257d8a3c3c13e82649bd302f73788c5
Pablo Papalardo <papalardo94@hotmail.com>
2018-12-02 19:45:10 -0200

    Supermarket

--- app/Models/Supermarket.php
+++ app/Models/Supermarket.php
@@ -11,6 +11,6 @@ class Supermarket extends Model
     ];
 
     public function products() {
-        return $this->hasMany(Products::class);
+        return $this->hasMany(Product::class);
     }
 }
